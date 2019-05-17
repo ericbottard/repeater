ebe9df6d0f8f414f0bab77a8c84fdc956fa1aeba
Florent Biville <fbiville@pivotal.io>
2019-05-17 16:20:25 +0200

    Example of shared fluxes

--- src/main/java/com/acme/Repeater.java
+++ src/main/java/com/acme/Repeater.java
@@ -1,31 +1,25 @@
 package com.acme;
 
 import reactor.core.publisher.Flux;
-import reactor.math.MathFlux;
 
-import java.io.IOException;
-import java.time.Duration;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.stream.IntStream;
 
 public class Repeater implements BiFunction<Flux<String>, Flux<Integer>, Flux<?>[]> {
 
     @Override
     public Flux<?>[] apply(Flux<String> stringFlux, Flux<Integer> integerFlux) {
 
+        Flux<Integer> sharedIntFlux = integerFlux.publish().autoConnect(2);
 
-        Flux<String> repeated = stringFlux.zipWith(integerFlux)
+        Flux<String> repeated = stringFlux.zipWith(sharedIntFlux)
                 .doOnNext(System.out::println)
-                .flatMap(t -> Flux.fromIterable(Collections.nCopies(t.getT2(), t.getT1()))
-                );
+                .flatMap(t -> Flux.fromIterable(Collections.nCopies(t.getT2(), t.getT1())));
 
-        //Flux<Integer> sum = integerFlux.buffer(3, 1).map(l -> l.stream().mapToInt(Integer::intValue).sum());
+        Flux<Integer> sum = sharedIntFlux.buffer(3, 1).map(l -> l.stream().mapToInt(Integer::intValue).sum()).take(10);
         //Flux<Integer> sum = Flux.interval(Duration.ofMillis(500)).map(Long::intValue);
 
-        return new Flux[]{repeated/*, sum*/};
+        return new Flux[]{repeated, sum};
     }
 
 }
