4072dd07a80a09b3fb6f417e64512ad00ec52f61
Eric Bottard <ebottard@pivotal.io>
2019-05-20 13:55:13 +0200

    Remove take for demo

--- src/main/java/com/acme/Repeater.java
+++ src/main/java/com/acme/Repeater.java
@@ -7,19 +7,20 @@ import java.util.function.BiFunction;
 
 public class Repeater implements BiFunction<Flux<String>, Flux<Integer>, Flux<?>[]> {
 
-    @Override
-    public Flux<?>[] apply(Flux<String> stringFlux, Flux<Integer> integerFlux) {
+	@Override
+	public Flux<?>[] apply(Flux<String> stringFlux, Flux<Integer> integerFlux) {
 
-        Flux<Integer> sharedIntFlux = integerFlux.publish().autoConnect(2);
+		Flux<Integer> sharedIntFlux = integerFlux.publish().autoConnect(2);
 
-        Flux<String> repeated = stringFlux.zipWith(sharedIntFlux)
-                .doOnNext(System.out::println)
-                .flatMap(t -> Flux.fromIterable(Collections.nCopies(t.getT2(), t.getT1())));
+		Flux<String> repeated = stringFlux
+				.zipWith(sharedIntFlux)
+				.flatMap(t -> Flux.fromIterable(Collections.nCopies(t.getT2(), t.getT1())));
 
-        Flux<Integer> sum = sharedIntFlux.buffer(3, 1).map(l -> l.stream().mapToInt(Integer::intValue).sum()).take(10);
-        //Flux<Integer> sum = Flux.interval(Duration.ofMillis(500)).map(Long::intValue);
+		Flux<Integer> sum = sharedIntFlux
+				.buffer(3, 1)
+				.map(l -> l.stream().mapToInt(Integer::intValue).sum());
 
-        return new Flux[]{repeated, sum};
-    }
+		return new Flux[] { repeated, sum };
+	}
 
 }
