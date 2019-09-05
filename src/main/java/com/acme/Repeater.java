0937f51a2dbc179b8440ce45b6dda382f525be9d
Eric Bottard <ebottard@pivotal.io>
2019-09-05 15:28:18 +0200

    Switch to Function of Tuples

--- src/main/java/com/acme/Repeater.java
+++ src/main/java/com/acme/Repeater.java
@@ -1,26 +1,26 @@
 package com.acme;
 
 import reactor.core.publisher.Flux;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
 import java.util.Collections;
-import java.util.function.BiFunction;
+import java.util.function.Function;
 
-public class Repeater implements BiFunction<Flux<String>, Flux<Integer>, Flux<?>[]> {
+public class Repeater implements Function<Tuple2<Flux<String>, Flux<Integer>>, Tuple2<Flux<String>, Flux<Integer>>> {
 
-	@Override
-	public Flux<?>[] apply(Flux<String> stringFlux, Flux<Integer> integerFlux) {
+    @Override
+    public Tuple2<Flux<String>, Flux<Integer>> apply(Tuple2<Flux<String>, Flux<Integer>> inputs) {
+        Flux<String> stringFlux = inputs.getT1();
+        Flux<Integer> integerFlux = inputs.getT2();
+        Flux<Integer> sharedIntFlux = integerFlux.publish().autoConnect(2);
 
-		Flux<Integer> sharedIntFlux = integerFlux.publish().autoConnect(2);
+        Flux<String> repeated = stringFlux.zipWith(sharedIntFlux)
+                .flatMap(t -> Flux.fromIterable(Collections.nCopies(t.getT2(), t.getT1())));
 
-		Flux<String> repeated = stringFlux
-				.zipWith(sharedIntFlux)
-				.flatMap(t -> Flux.fromIterable(Collections.nCopies(t.getT2(), t.getT1())));
+        Flux<Integer> sum = sharedIntFlux.buffer(2, 1)
+                .map(l -> l.stream().mapToInt(Integer::intValue).sum());
 
-		Flux<Integer> sum = sharedIntFlux
-				.buffer(3, 1)
-				.map(l -> l.stream().mapToInt(Integer::intValue).sum());
-
-		return new Flux[] { repeated, sum };
-	}
-
+        return Tuples.of(repeated, sum);
+    }
 }
